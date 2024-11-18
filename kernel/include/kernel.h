7625c10b4e6e2abcc940de463aa33036f21110d5
Scooter <info@talkufy.com>
2024-11-18 12:46:59 -0500

    Implementing the Interrupt Descriptor Table, and the basis for IO

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -1,10 +1,20 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
+#include <stdint.h>
+
 void init_gdt();
 void init_idt();
 void init_memory();
 
 void kernel_main();
+
+void set_idt_gate(uint8_t num, uint64_t handler, uint8_t ist, uint8_t flags);
+void idt_flush(uint64_t);
+
+void outb(uint16_t port, uint8_t val);
+uint8_t inb(uint16_t port);
+
+void enable_interrupts();
 
 #endif
