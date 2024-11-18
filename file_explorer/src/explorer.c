ded363814e110c4734c58615b21725e9d206aa22
Scooter <info@talkufy.com>
2024-11-18 10:48:16 -0500

    Pretty much just added the MAKEFILE and the basis for my explorer and window_manager

---
+++ file_explorer/src/explorer.c
@@ -0,0 +1,9 @@
+#include "explorer.h"
+
+void explorer_init() {
+        // Initialize file explorer
+}
+
+void display_directory(const char* path) {
+    // TODO: Implement the directory display
+} 
---
+++ gui/src/window_manager.c
@@ -0,0 +1,9 @@
+#include "window_manager.h"
+
+void window_manager_init() {
+    // TODO: Implement the window manager
+}
+
+void render_window(const char* title) {
+    // TODO: Implement the window rendering
+}
